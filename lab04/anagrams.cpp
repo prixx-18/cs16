660c14138ee63ad4ed5f48d7dcdbf4390752ea7d
Priyansh Nath <pnath@csilvm-20.cs.ucsb.edu>
2026-02-04 14:55:09 -0800

    Annotated codes

--- lab04/anagrams.cpp
+++ lab04/anagrams.cpp
@@ -3,53 +3,57 @@
 #include <cctype>
 using namespace std;
 
-// MISSING FUNCTION DECLARATIONS HERE (you can remove these comments)
+// Function declarations
 string cleanString(string &string1);
 void anagramCheck(string string1, string string2);
 
 int main()
 {
-    // MISSING CODE HERE (you can remove these comments)
-	// Get strings from user -- USE:   getline(cin, <string_variable>);
-	// Clean strings
-	// Check to see if they are anagrams
-	// Report if they are or not anagrams
 	string string1, string2;
 
-	getline(cin, string1);
+	//Get strings from user
+	getline(cin, string1); 
 	getline(cin, string2);
 
+	// Cleans string for anagram check
 	cleanString(string1);
 	cleanString(string2);
 	
+	//Completes anagram check and prints verdict
 	anagramCheck(string1, string2);
 	
 	return 0;
 }
 
 
-// MISSING FUNCTION DEFINITIONS HERE (you can remove these comments)
+// Precondition: Takes in a string passed by reference to physically change the string for later use. Called in anagramCheck.
+// Post Condition: Returns a cleaned string stripped of non-alphabetical characters and all letters are lower cased
 string cleanString(string &string1)
 {
-	string new_string = "";
+	string new_string = ""; // New string is created to store clean characters
 	for (int i = 0; i < string1.length(); i++){
-		if (isalpha(string1[i]))
-			new_string += tolower(string1[i]);
+		if (isalpha(string1[i])) 
+			new_string += tolower(string1[i]); // Appends all alphabetical characters lower cased
 	}
 	string1 = new_string;
 	return string1;
 }
 
+// Precondition: Takes in two strings that you want to compare (calls cleanString automatically)
+// Post Condition: Prints whether the strings are anagrams or not
 void anagramCheck(string string1, string string2)
 {
+	// Calls clean string for the user
 	cleanString(string1);
 	cleanString(string2);
 
+	// Automatically decides strings aren't anagrams if lengths are different post clean
 	if (string1.length() != string2.length()) {
         	cout << "The strings are not anagrams" << endl;
 		return;
     	}
 	
+	// used_index array of zeros to track when a letter index was already matched 
 	const int size = string1.length();
     	int used_index[size];
     
@@ -61,6 +65,7 @@ void anagramCheck(string string1, string string2)
 		bool foundMatch = false;
         
 		for (int j = 0; j < size; j++) {
+			// Match is found if string 1 and 2 have the same letter and an match wasn't already made in same index
 			if ((string1[i] == string2[j]) && (used_index[j] == 0)) {
 				used_index[j] = 1;
                 		foundMatch = true;
@@ -68,6 +73,7 @@ void anagramCheck(string string1, string string2)
             		}
         	}
         
+	// If any letter is off, foundMatch is false
         if (!foundMatch) {
 		cout << "The strings are not anagrams" << endl;
 		return;
--- lab04/stats.cpp
+++ lab04/stats.cpp
@@ -9,47 +9,51 @@ double average(int *array, int size);
 double median(int* array, int size);
 double stddev(int* array, int size);
 
-int main() {
-    // Sets the printing of floating-point numbers
-    // to show only 2 places after the decimal point
-    cout << fixed << showpoint;
-    cout << setprecision(2);
+int main()
+{
+	// Sets the printing of floating-point numbers
+    	// to show only 2 places after the decimal point
+	cout << fixed << showpoint;
+	cout << setprecision(2);
 
-    // MISSING CODE HERE (you can remove these comments)
-	// Get the number of grades to be entered from user
-    // Create a dynamic int array based on this information
-    // Get the user to enter all the grades
-    // Print results while calling the appropriate functions
+	// Initialize int pointer pointing to Null so it's not dangling    
+	int *grades = nullptr;
+	
+	// Get size from user
+	unsigned int size;
+	cout << "Enter number of grades: ";
+	cin >> size;
+	
+	// Reject sizes less than 1
+	if (size < 1){
+		cerr << "Error!\n";
+		exit(1);
+	}
 
-    int *grades = nullptr;
-    unsigned int size;
-    cout << "Enter number of grades: ";
-    cin >> size;
-    if (size < 1){
-	cerr << "Error!\n";
-	exit(1);
-    }
+	// Create dynamic array in freestore/heap and have user enter each element
+	grades = new int[size];
+	cout << "Enter grades (each on a new line):" << endl;
+	for (unsigned int i = 0; i < size; i++) {
+        	cin >> grades[i];
+    	}
 
-    grades = new int[size];
-    cout << "Enter grades (each on a new line):" << endl;
-    for (unsigned int i = 0; i < size; i++) {
-        cin >> grades[i];
-    }
+	// Call bubbleSort to sort the array and average, median, and stddev to calculate respective stats
+	bubbleSort(grades, size);
+	double mean = average(grades, size);
+	double medVal = median(grades, size);
+	double StdDev = stddev(grades, size);
+	
+	// Print statistics and align using setw()
+	cout << "Here are some statistics:" << endl;
+	cout << setw(9) << "Average: " << mean << endl;
+	cout << setw(9) << "Median: " << medVal << endl;
+	cout << setw(9) << "StdDev: " << StdDev << endl;
 
-    bubbleSort(grades, size);
-    double mean = average(grades, size);
-    double medVal = median(grades, size);
-    double StdDev = stddev(grades, size);
+	// Delete dynamic array to return mem to heap and prevent mem leaks. Reassign pointer to Null
+	delete []grades;
+	grades = nullptr;
 
-    cout << "Here are some statistics:" << endl;
-    cout << setw(9) << "Average: " << mean << endl;
-    cout << setw(9) << "Median: " << medVal << endl;
-    cout << setw(9) << "StdDev: " << StdDev << endl;
-
-    delete []grades;
-    grades = nullptr;
-
-    return 0;
+	return 0;
 }
 
 // This is bubbleSort - it is given for you to use.
@@ -68,38 +72,48 @@ void bubbleSort(int *array, int size) {
   } // for i
 }
 
-// MISSING FUNCTION DEFINITIONS HERE (you can remove these comments)
+// Pre-condition: Takes in a pointer to an array (dynamic for this code) and the array size
+// Post-condition: Returns the mean value of the array as a double 
 double average(int *array, int size)
 {
-	double sum = 0.0;
+	double sum = 0.0; // Force floating point math
 	for (int i = 0; i < size; i++)
 		sum += *(array + i); 
 
 	return sum / size;
 }
 
+// Pre-condition: Takes in a pointer to an array (dynamic for this code) and the array size
+// Post-condition: Returns the median value of the array as a double
 double median(int* array, int size)
 {
-	if (size % 2 != 0){
-		return *(array + (size / 2));
+	if (size % 2 != 0){ 
+		return *(array + (size / 2)); // Return the middle value of an odd sized array
 	} else {
 		int mid1 = *(array + (size / 2));
 		int mid2 = *(array + ((size / 2 ) - 1));
-		return (mid1 + mid2) / 2.0;
+		return (mid1 + mid2) / 2.0; // Return the average of the two middle numbers in an even sized array
 	}
 }
 
-double stddev(int* array, int size){
+// Pre-condition: Takes in a pointer to an array (dynamic for this code) and the array size
+// Post-condition: Returns the standard deviation value of the array as a double
+double stddev(int* array, int size)
+{
+	// stddev is always 0 when size is 1
 	if (size == 1)
 		return 0.0;
 
 	double summand = 0.0;
 	
+	// Call average to use mean in stddev calculations
 	double mean = average(array, size);
 	
+	// Add every element's euclidean distance from mean to simulate summation
 	for (int i = 0; i < size; i++)
 		summand += pow(*(array + i) - mean, 2);
 
+	// Raise to power of 0.5 to square root the expression
 	return pow((1.0 / (size - 1)) * summand, 0.5);
 
 }
