7a55caca0fd1713a1fe5511aca2f309e1b146c36
Priyansh Nath <pnath@csilvm-06.cs.ucsb.edu>
2026-02-09 11:35:03 -0800

    Adding vectorSplit.cpp to repo

---
+++ lab05/vectorSplit.cpp
@@ -0,0 +1,88 @@
+#include <iostream> // for cin, cout
+#include <vector>   // for vector
+#include <string>   // for getline()
+// YOU ARE NOT ALLOWED TO INCLUDE ANY OTHER LIBRARY!!! (zero grade penalty)
+using namespace std;
+
+// FUNCTION DECLARATION: YOU MUST DEFINE AND USE THIS (do not remove):
+//      You can declare/define other functions if you like as well,
+//      but you must use function split() below.
+vector<string> split(string target, string delimiter);
+void printVector(vector<string> stringVector);
+
+int main() {
+
+    // Initialize string variables
+    	string target;
+	string delimiter;
+
+    // Get target string from user, get delimiter string from user
+	cout << "Enter string to split:" << endl;
+	getline(cin, target); 
+	cout << "Enter delimiter string" << endl;
+	getline(cin, delimiter);
+
+    // Call function split()
+    	vector<string> splitString = split(target, delimiter);
+    
+    // Print results
+	cout << "The subtrings are: ";
+    	printVector(splitString);
+
+    return 0;
+}
+
+// Precondition: Takes two string arguments, the main target string and the string you want to split the target by
+// Postcondition: Returns a vector of substrings that are split by the desired delimiter
+vector<string> split(string target, string delimiter)
+{
+    vector<string> substrings;
+    
+    // Exits the function if string is empty
+    if (target == "")
+    {
+        cerr << "No substrings." << endl;
+        exit(1);
+    }
+
+    // String variable constructed for every substring between delimiters
+    string substring = "";
+    for (unsigned int i = 0; i < target.size(); i++)
+    {
+        // Cuts off substring if delimiter is detected
+        if (target[i] == delimiter[0])
+        {
+            // Only pushes the substring if the substring isn't empty
+            if (substring != "")
+            {
+                substrings.push_back(substring);
+                substring = "";
+            }
+        }
+        else
+            // Concatonates character if it isn't delimiter
+            substring += target[i];
+    }
+
+    // Pushes last substring if it isn't empty
+    if (substring != "")
+        substrings.push_back(substring);
+
+    return substrings;
+}
+
+// Takes in a vector of strings
+// Prints the vector with each string encapsulated in "" and separated by commas
+void printVector(vector<string> stringVector)
+{
+	for (unsigned int i = 0; i < stringVector.size(); i++)
+	{
+		// Omits comma from last substring printed
+		if (i == stringVector.size() - 1)
+		{
+			cout << "\"" << stringVector[i] << "\"" << endl;
+			return;
+		}
+	cout << "\"" << stringVector[i] << "\", ";
+	}
+}
